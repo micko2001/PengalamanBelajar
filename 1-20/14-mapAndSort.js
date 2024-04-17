260846de2875786b300556137348cd9bb9671c3c
micko2001 <micko.wijayanto57@gmail.com>
2024-04-17 08:36:17 +0700

    filter vs map

--- 1-20/14-mapAndSort.js
+++ 1-20/14-mapAndSort.js
@@ -1,14 +1,14 @@
-let arr = [1,3,2,10,6,4,7,9];
+let arr = [1, 3, 2, 10, 6, 4, 7, 9];
 
-const arr2=arr.map(function(e){
-    console.log(e);
-    return e+2;
+const arr2 = arr.map(function (e) {
+  console.log(e);
+  return e + 2;
 });
 let numbers = [1, 2, 3, 4, 5];
 
 let sum = 0;
 
-numbers.forEach(function(number) {
+numbers.forEach(function (number) {
   sum += number;
 });
 
@@ -17,30 +17,54 @@ console.log("Sum of numbers:", sum);
 
 console.log(arr2.join("-"));
 
-
 // let arr1=arr.sort();
 // console.log(arr1);
 
-arr.sort(function(a,b){
-    return a-b;
-})
-
+arr.sort(function (a, b) {
+  return a - b;
+});
 
 console.log(arr);
 arr.push(3);
-let s=arr.filter(function(x){return x!=3;})
+let s = arr.filter(function (x) {
+  return x != 3;
+});
 console.log(s);
 console.log(arr);
-let fd= arr.find(function(x){
-    return x==3;
-})
+let fd = arr.find(function (x) {
+  return x == 3;
+});
 console.log(fd);
 
-const newArr =[-9,2,4,1,5,6,-4,-7,3];
+const newArr = [-9, 2, 4, 1, 5, 6, -4, -7, 3];
 
-console.log(newArr.filter(e=>e>0));//output [ 2, 4, 1, 5, 6, 3 ]
-let isi=0;
-console.log([1,1,2].sort((a,b)=>a-b).filter((a,b,c)=>a!=c[b+1]));
+console.log(newArr.filter((e) => e > 0)); //output [ 2, 4, 1, 5, 6, 3 ]
+let isi = 0;
+console.log([1, 1, 2].sort((a, b) => a - b).filter((a, b, c) => a != c[b + 1]));
 
-const arr3=[1,3];
-console.log(arr3.reduce(e=>e%2==0?true:false));
+const arr3 = [1, 3];
+console.log(arr3.reduce((e) => (e % 2 == 0 ? true : false)));
+
+// map()
+/*
+Creates a new array with the same length as the 
+original array, but with each element transformed 
+by the callback function. 
+
+Used when you want to transform each element in an array
+
+Returns a new array with the same length as the original array.
+
+*/
+
+//filter()
+/*
+Creates a new array with only the elements that 
+pass the conditions implemented by the callback function.
+
+Used when you want to select only certain elements that 
+meet a specific condition.
+
+Returns a new array with a length that is equal to or 
+less than the original array.
+*/
